c2561f13621edca10a09efce7d5a0b95b11bb300
Jimel J. Joseph <jaydev@Jimels-MacBook-Pro-2.local>
2017-03-09 18:39:40 -0500

    layout set

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+var diamond1 = 0;
+var diamond2 = 0;
+var diamond3 = 0;
+var diamond4 = 0;
+var randomNumber = 0;
+var wins = 0;
+var losses = 0;
+var userTotalScore = 0;
+
+randomNumber = Math.floor(Math.random() * 101)+19;
+console.log(randomNumber);
+$("#randomNumber").html(randomNumber)
+
+$("")
